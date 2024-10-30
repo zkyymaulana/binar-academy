9e3d93d452804ec1cf0e67500df7fc34e20227b9
M. Zaky Pria Maulana <zakymaulana363@gmail.com>
2024-10-30 09:14:39 +0700

    updated crud chapter 5

--- chapter5/express-js/src/index.js
+++ chapter5/express-js/src/index.js
@@ -1,34 +1,38 @@
-require('dotenv').config(); // To enable .env called
-const express = require('express'); // Import express with non-module
-require('express-async-errors');
-const fileUpload = require('express-fileupload'); // This package is to enable req.files
-const router = require('./routes');
-const { errorHandler, notFoundURLHandler } = require('./middlewares/errors');
+require("dotenv").config(); // To enable .env called
+const express = require("express"); // Import express with non-module
+require("express-async-errors");
+const fileUpload = require("express-fileupload"); // This package is to enable req.files
+const cors = require("cors");
+const router = require("./routes");
+const { errorHandler, notFoundURLHandler } = require("./middlewares/errors");
 
-/* Make/initiate express application */
+/* Make/initiate expess application */
 const app = express();
 const port = process.env.PORT || 4000;
 
-/* We need to activate body parser/reader */
+/* Enable CORS */
+app.use(cors());
+
+/* We need to activate body parser/reader (req.body) */
 app.use(express.json());
 
 /* We need to read form-body (body parser/reader) (req.files) if you want upload file */
 app.use(
-	fileUpload({
-		limits: { fileSize: 50 * 1024 * 1024 }, // 50mb
-	})
+    fileUpload({
+        limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
+    })
 );
 
 // All routes define here
-app.use('/', router);
+app.use("/", router);
 
 // This function is for 404 handle URL
-app.use('*', notFoundURLHandler);
+app.use("*", notFoundURLHandler);
 
 // This function is to handle error when API hit, it always be the last middleware
 app.use(errorHandler);
 
 /* Run the express.js application */
 app.listen(port, () => {
-	console.log(`The express.js app is running on port ${port}`);
+    console.log(`The express.js app is runing on port ${port}`);
 });
--- chapter5/express-js/src/utils/image-kit.js
+++ chapter5/express-js/src/utils/image-kit.js
@@ -1,16 +1,17 @@
-const ImageKit = require('imagekit');
+const ImageKit = require("imagekit");
 
+// Image kit init
 const imagekit = new ImageKit({
-	publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-	privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-	urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 // Image upload function to imagekit
-exports.imageUpload = async file => {
-	const uploadFile = await imagekit.upload({
-		file: file.data,
-		fileName: file.name,
-	});
-	return uploadFile?.url;
+exports.imageUpload = async (file) => {
+    const uploadedFile = await imagekit.upload({
+        file: file.data,
+        fileName: file.name,
+    });
+    return uploadedFile?.url;
 };
